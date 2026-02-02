58c727d5632f4ccbbba6a4794490cfe0329994af
Deepak9669 <deepakverma726800@gmail.com>
2026-02-02 17:37:24 +0530

    done

---
+++ spring-core-project/src/main/java/com/rays/test/TestPerson.java
@@ -0,0 +1,28 @@
+package com.rays.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("user.xml"));
+
+		Person dto = (Person) factory.getBean("person");
+		
+		System.out.println(dto.getName());
+		System.out.println(dto.getLogin());
+		System.out.println(dto.getPassword());
+		
+		System.out.println("<---------------->");
+		
+		 dto = (Person) factory.getBean("person1");
+			
+			System.out.println(dto.getName());
+			System.out.println(dto.getLogin());
+			System.out.println(dto.getPassword());
+	}
+
+}
